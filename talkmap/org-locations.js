7202f7b644f43d788ef631aca522c9012d1903d9
rkclement <rclement@middlebury.edu>
2018-06-28 21:57:43 -0400

    improving talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,47 +1,57 @@
 var addressPoints = [
   [
-    "San Diego, CA",
+    "The blueprint as a map: designing better data services with service blueprinting | Research Data Access & Preservation Summit , San Diego, CA",
     32.7174209,
     -117.1627714
   ],
   [
-    "St. Paul, MN",
+    "Beyond finding and managing: Extending research data services at liberal arts institutions | Oberlin Digital Scholarship Conference, St. Paul, MN",
     44.9504037,
     -93.1015026
   ],
   [
-    "Portland, OR",
+    "Where's the data? Building data services for liberal arts colleges. | ACRL Conference, Portland, OR",
     45.5202471,
     -122.6741949
   ],
   [
-    "Detroit, MI",
+    "Reed College Dataverse pilot project | Pacific Northwest Data Curators Annual Meeting, Portland, OR",
+    45.5202471,
+    -122.6741949
+  ],
+  [
+    "The DIY conference: A communities of practice model for library student development | Michigan Library Association Annual Conference, Detroit, MI",
     42.3486635,
     -83.0567375
   ],
   [
-    "Eugene, OR",
+    "Think like a scholar: Using the ACRL Framework to shape collaborative, discipline-based information literacy instruction | Oregon Library Association Annual Conference, Eugene, OR",
     44.0505054,
     -123.0950506
   ],
   [
-    "Lawrence, KS",
+    "You can too! Running a successful data bootcamp for novices | IASSIST 2017, Lawrence, KS",
     38.9719384,
     -95.2359496
   ],
   [
-    "Pittsburgh, PA",
+    "Library & laboratory: institutionalizing\na dynamic digital projects infrastructure\nacross workgroups. | Digital Library Federation Forum 2017, Pittsburgh, PA",
     40.4416941,
     -79.9900861
   ],
   [
-    "Vancouver, BC",
+    "NW5C research data management & curation workshop: A collaborative model for liberal arts colleges | Digital Library Federation Forum Liberal Arts Pre-conference 2015, Vancouver, BC",
     49.2608724,
     -123.1139529
   ],
   [
-    "Atlanta, GA",
+    "Measuring up: How are we defining success for research data services? | Research Data Access & Preservation Summit , Atlanta, GA",
     33.7490987,
     -84.3901849
+  ],
+  [
+    "Looking at the library data in the mirror: taking our own advice about data management | IASSIST 2017, Lawrence, KS",
+    38.9719384,
+    -95.2359496
   ]
 ];
