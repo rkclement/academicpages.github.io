f427976f631ce33d162bd361ed39e1a15f25127f
rkclement <rclement@middlebury.edu>
2018-06-28 22:25:57 -0400

    improving talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -53,5 +53,60 @@ var addressPoints = [
     "<b>Looking at the library data in the mirror: taking our own advice about data management</b></br>IASSIST 2017, Lawrence, KS",
     38.9719384,
     -95.2359496
+  ],
+  [
+    "<a href=/talks/2014-03-27-poster-1>The blueprint as a map: designing better data services with service blueprinting</a></br>Research Data Access & Preservation Summit, San Diego, CA",
+    32.7174209,
+    -117.1627714
+  ],
+  [
+    "<a href=/talks/2016-06-11-tutorial-2>Beyond finding and managing: Extending research data services at liberal arts institutions</a></br>Oberlin Digital Scholarship Conference, St. Paul, MN",
+    44.9504037,
+    -93.1015026
+  ],
+  [
+    "<a href=/talks/2015-3-27-talk-8>Where's the data? Building data services for liberal arts colleges.</a></br>ACRL Conference, Portland, OR",
+    45.5202471,
+    -122.6741949
+  ],
+  [
+    "<a href=/talks/2015-06-23-talk-9>Reed College Dataverse pilot project</a></br>Pacific Northwest Data Curators Annual Meeting, Portland, OR",
+    45.5202471,
+    -122.6741949
+  ],
+  [
+    "<a href=/talks/2012-11-08-talk-10>The DIY conference: A communities of practice model for library student development</a></br>Michigan Library Association Annual Conference, Detroit, MI",
+    42.3486635,
+    -83.0567375
+  ],
+  [
+    "<a href=/talks/2015-04-17-talk-7>Think like a scholar: Using the ACRL Framework to shape collaborative, discipline-based information literacy instruction</a></br>Oregon Library Association Annual Conference, Eugene, OR",
+    44.0505054,
+    -123.0950506
+  ],
+  [
+    "<a href=/talks/2017-05-23-tutorial-1>You can too! Running a successful data bootcamp for novices</a></br>IASSIST 2017, Lawrence, KS",
+    38.9719384,
+    -95.2359496
+  ],
+  [
+    "<a href=/talks/2017-10-24-talk-1>Library & laboratory: institutionalizing\na dynamic digital projects infrastructure\nacross workgroups.</a></br>DLF Forum 2017, Pittsburgh, PA",
+    40.4416941,
+    -79.9900861
+  ],
+  [
+    "<a href=/talks/2015-10-25-talk-6>NW5C research data management & curation workshop: A collaborative model for liberal arts colleges</a></br>DLF Forum Liberal Arts Pre-conference 2015, Vancouver, BC",
+    49.2608724,
+    -123.1139529
+  ],
+  [
+    "<a href=/talks/2016-05-05-talk-5>Measuring up: How are we defining success for research data services?</a></br>Research Data Access & Preservation Summit, Atlanta, GA",
+    33.7490987,
+    -84.3901849
+  ],
+  [
+    "<a href=/talks/2017-05-26-talk-2>Looking at the library data in the mirror: taking our own advice about data management</a></br>IASSIST 2017, Lawrence, KS",
+    38.9719384,
+    -95.2359496
   ]
 ];
