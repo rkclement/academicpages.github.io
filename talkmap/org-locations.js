d19f8423d02ed4feef3099c2f4d13bd385a1b618
rkclement <rclement@middlebury.edu>
2018-06-28 22:16:30 -0400

    improving talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,56 +1,56 @@
 var addressPoints = [
   [
-    "The blueprint as a map: designing better data services with service blueprinting | Research Data Access & Preservation Summit , San Diego, CA",
+    "<b>The blueprint as a map: designing better data services with service blueprinting</b></br>Research Data Access & Preservation Summit, San Diego, CA",
     32.7174209,
     -117.1627714
   ],
   [
-    "Beyond finding and managing: Extending research data services at liberal arts institutions | Oberlin Digital Scholarship Conference, St. Paul, MN",
+    "<b>Beyond finding and managing: Extending research data services at liberal arts institutions</b></br>Oberlin Digital Scholarship Conference, St. Paul, MN",
     44.9504037,
     -93.1015026
   ],
   [
-    "Where's the data? Building data services for liberal arts colleges. | ACRL Conference, Portland, OR",
+    "<b>Where's the data? Building data services for liberal arts colleges.</b></br>ACRL Conference, Portland, OR",
     45.5202471,
     -122.6741949
   ],
   [
-    "Reed College Dataverse pilot project | Pacific Northwest Data Curators Annual Meeting, Portland, OR",
+    "<b>Reed College Dataverse pilot project</b></br>Pacific Northwest Data Curators Annual Meeting, Portland, OR",
     45.5202471,
     -122.6741949
   ],
   [
-    "The DIY conference: A communities of practice model for library student development | Michigan Library Association Annual Conference, Detroit, MI",
+    "<b>The DIY conference: A communities of practice model for library student development</b></br>Michigan Library Association Annual Conference, Detroit, MI",
     42.3486635,
     -83.0567375
   ],
   [
-    "Think like a scholar: Using the ACRL Framework to shape collaborative, discipline-based information literacy instruction | Oregon Library Association Annual Conference, Eugene, OR",
+    "<b>Think like a scholar: Using the ACRL Framework to shape collaborative, discipline-based information literacy instruction</b></br>Oregon Library Association Annual Conference, Eugene, OR",
     44.0505054,
     -123.0950506
   ],
   [
-    "You can too! Running a successful data bootcamp for novices | IASSIST 2017, Lawrence, KS",
+    "<b>You can too! Running a successful data bootcamp for novices</b></br>IASSIST 2017, Lawrence, KS",
     38.9719384,
     -95.2359496
   ],
   [
-    "Library & laboratory: institutionalizing\na dynamic digital projects infrastructure\nacross workgroups. | Digital Library Federation Forum 2017, Pittsburgh, PA",
+    "<b>Library & laboratory: institutionalizing\na dynamic digital projects infrastructure\nacross workgroups.</b></br>DLF Forum 2017, Pittsburgh, PA",
     40.4416941,
     -79.9900861
   ],
   [
-    "NW5C research data management & curation workshop: A collaborative model for liberal arts colleges | Digital Library Federation Forum Liberal Arts Pre-conference 2015, Vancouver, BC",
+    "<b>NW5C research data management & curation workshop: A collaborative model for liberal arts colleges</b></br>DLF Forum Liberal Arts Pre-conference 2015, Vancouver, BC",
     49.2608724,
     -123.1139529
   ],
   [
-    "Measuring up: How are we defining success for research data services? | Research Data Access & Preservation Summit , Atlanta, GA",
+    "<b>Measuring up: How are we defining success for research data services?</b></br>Research Data Access & Preservation Summit, Atlanta, GA",
     33.7490987,
     -84.3901849
   ],
   [
-    "Looking at the library data in the mirror: taking our own advice about data management | IASSIST 2017, Lawrence, KS",
+    "<b>Looking at the library data in the mirror: taking our own advice about data management</b></br>IASSIST 2017, Lawrence, KS",
     38.9719384,
     -95.2359496
   ]
